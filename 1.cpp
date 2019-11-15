428a589191fcd753601d00d6530b6ecd884bb08b
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-11-15 14:40:48 +0800

    feat: backup

--- 1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<ctime>
-using namespace std;
-int main(void){
-  time_t now = time(NULL);
-  cout << now << endl
-       << now - 1 << endl
-       << (now >1) << endl;
-}
